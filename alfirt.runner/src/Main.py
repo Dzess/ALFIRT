d13eab8e009f2b3827ba4a1d580b165a6f37dd62
Piotr Jessa <dzesse@gmail.com>
2011-05-04 15:29:17 +0200

    Initial stubs of files

---
+++ alfirt.runner/src/Main.py
@@ -0,0 +1,13 @@
+'''
+Created on 04-05-2011
+
+Entry point for ALFIRT project. Runs the tests specified in 
+command line.
+
+@author: Piotr
+'''
+
+if __name__ == '__main__':
+    print "Welcome to ALFIRT project"
+    
+    pass
---
+++ alfirt.verifier/src/Main.py
@@ -0,0 +1,20 @@
+'''
+Created on 04-04-2011
+Main entry point for ALFIRT Verifier project.
+Uses the native Python 2.7 x64 enviorment. Loads the data from 
+X3D input file tagged with ALFIRT tags and loads the output data from classifier.
+ 
+
+@author: Piotr Jessa
+'''
+
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print "Usage message should be printed here"
+        sys.exit(1)
+        
+    #TODO write reading line from system argv, and invoking good classes for that
+    
+    pass
