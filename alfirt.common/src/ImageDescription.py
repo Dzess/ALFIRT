6c72b6f3d96559b7e43945a5e3e1c898a53df5e3
Piotr Jessa <dzesse@gmail.com>
2011-05-04 20:28:33 +0200

    Fixing git ignore, adding stubs for tests in common

---
+++ alfirt.common/src/ImageDescription.py
@@ -0,0 +1,19 @@
+'''
+Created on 04-05-2011
+
+@author: Piotr
+'''
+
+class ImageDescription(object):
+    '''
+    Class represents the image characteristics from the 
+    learning point of view.
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+        
