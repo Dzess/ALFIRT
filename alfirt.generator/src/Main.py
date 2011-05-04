d6943d1a472d037dcda3c7e0a749ce3b1fc3a314
Mikolaj Dobski <ankhazam@gmail.com>
2011-05-04 22:27:32 +0200

    Stub...

---
+++ alfirt.generator/src/Main.py
@@ -0,0 +1,11 @@
+'''
+Created on 04-05-2011
+
+Tool for creating renders with description from given model template.
+
+@author: Miko³aj Dobski
+'''
+
+if __name__ == '__main__':
+    print "We will survive!"
+    pass
