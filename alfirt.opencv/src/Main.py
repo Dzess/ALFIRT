cf0205b7aaeb38118f37ce288ea7c0f5c4d6cee7
Mikolaj Dobski <ankhazam@gmail.com>
2011-05-11 22:20:45 +0200

    file properties fixed, opencv loader stub with arg parser.

--- alfirt.opencv/src/Main.py
+++ alfirt.opencv/src/Main.py
@@ -5,13 +5,37 @@ Created on 05-05-2011
 '''
 
 import cv
+import sys
+from optparse import OptionParser
 
 if __name__ == '__main__':
-    print "OpenCV Test"
+    print "OpenCV Learning Application"
     
-    cv.NamedWindow("a_window", cv.CV_WINDOW_AUTOSIZE)
-    image=cv.LoadImage("D:\Downloads\prezent.png", cv.CV_LOAD_IMAGE_COLOR) #Load the image
-    font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 3, 8) #Creates a font
-    cv.ShowImage("a_window", image) #Show the image
+    parser = OptionParser(usage="usage: %prog [options] [image_filename] [expected_output_file]")
+    parser.add_option("-r", "--runType", action="store", dest="runType", type="str",
+        help="Run selection learn|test. If \"learn\" is selected then expected output file path has to be provided.",
+        default="test")
+    (options, args) = parser.parse_args()
+    
+    if ((options.runType != "learn") & (options.runType != "test")):
+        print "Invalid argument for -r option: " +options.runType
+        sys.exit()
+    elif (options.runType == "test"):
+        if len(args)!= 1:
+            print "Required path to image file missing."
+            sys.exit()
+        print "Testing mode"
+    else:
+        if len(args)!= 2:
+            print "Required paths to image and expected output files missing."
+            sys.exit()
+        print "Learning mode"
+    
+    
+    # Running opencv
+    window_name = options.runType
+    cv.NamedWindow(window_name, cv.CV_WINDOW_AUTOSIZE)
+    image=cv.LoadImage(args[0], cv.CV_LOAD_IMAGE_COLOR) #Load the image
+    cv.ShowImage(window_name, image) #Show the image
     cv.waitKey()
     pass
