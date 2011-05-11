637a913b9582df693c973fc8eb358c71d65a8270
Piotr Jessa <dzesse@gmail.com>
2011-05-11 20:23:23 +0200

    Caonfig file parser

---
+++ alfirt.runner/src/ConfigrReader.py
@@ -0,0 +1,100 @@
+'''
+Created on 10-05-2011
+
+@author: Piotr
+'''
+import unittest
+import os
+
+class ConfigReader(object):
+    '''
+    Configuration reader for the INI file, reading polar coordinates for image generation. 
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+
+    
+    def readFile(self, file):
+        '''
+        Reads file and crates and sets the values.
+        @param file: path to the file which has to be loaded with the configuration 
+        '''
+        raise ValueError()
+        pass
+    
+    
+    
+#===============================================================================
+#  UnitTests
+#===============================================================================
+class ConfiReaderUnitTests(unittest.TestCase):
+    
+    
+    def setUp(self):
+        
+        dataINIFormat = """[PolarCoordinates]
+AlfaStart: 20
+AlfaStop: 50
+BetaStart: 100
+BetaStop: 150
+RadiusStart: 10
+RadiusStop: 10
+
+[Others]
+Route: 66 
+"""
+        # Creating file
+        self.fileName ="test_file_name"
+        with open(self.fileName,'w') as fileStream:
+            fileStream.write(dataINIFormat)
+            fileStream.close()
+        pass
+    
+
+    def tearDown(self):
+        # Removing file after test
+        os.remove(self.fileName)
+        pass
+    
+    
+    def test_passing_good_values_results_in_data_creation(self):
+        '''
+        Checks when we pass good data, the configReader returns the structure with data
+        '''
+        # test reading
+        configReader = ConfigReader()
+        results = configReader.readFile(self.fileName)
+        
+        # add assertions about the resulted file
+        self.assertEqual(results.alfa.start,20, "Alfa start should be 20")
+        self.assertEqual(results.alfa.stop,50,"Alfa stop should be 50")
+        
+        self.assertEqual(results.beta.start, 100, "Beta start should be 100")
+        self.assertEqual(results.beta.stop, 150, "Beta stop should be 150")
+        
+        self.assertEqual(results.radius.start, 10, "Radius start should be 10")
+        self.assertEqual(results.radius.stop, 10, "Radius stop should be 10")
+        
+        pass
+    
+    def test_reading_empty_file_results_in_exception(self):
+        '''
+        Checks if the empty or None conformity file is acceptable 
+        '''
+        configReader = ConfigReader()
+        with self.assertRaises(ValueError):
+            configReader.readFile(None)
+    
+        with self.assertRaises(ValueError):
+            configReader.readFile("non_existing_file")
+        pass
+
+#===========================================================================
+#  Test runner
+#===========================================================================
+if (__name__ == 'main'):
+    unittest.main(verbosity=2)
