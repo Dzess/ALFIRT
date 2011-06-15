3575b08cfa1d13521a33fcbf7ca2ac6aa236c31b
Mikolaj Dobski <ankhazam@gmail.com>
2011-06-15 14:40:19 +0200

    pyopencv +chmod

--- alfirt.opencv/src/Main.py
+++ alfirt.opencv/src/Main.py
@@ -5,6 +5,7 @@ Created on 05-05-2011
 '''
 
 import cv
+from pyopencv import *
 from optparse import OptionParser
 import sys
 import ImageDescription
@@ -32,6 +33,35 @@ class NaiveRecognition(object):
         cv.ShowImage(window_name, image) #Show the image
         cv.waitKey()
         
+    def printGoodFeatures(self):
+        img = cv.LoadImageM(self.imagePath, cv.CV_LOAD_IMAGE_GRAYSCALE)
+        imgColour = cv.LoadImageM(self.imagePath)
+        eig_image = cv.CreateMat(img.rows, img.cols, cv.CV_32FC1)
+        temp_image = cv.CreateMat(img.rows, img.cols, cv.CV_32FC1)
+        
+        for (x, y) in cv.GoodFeaturesToTrack(img, eig_image, temp_image, 100, 0.04, 1.0, useHarris=True):
+            cv.Circle(imgColour, (int(x), int(y)), 3, cv.Scalar(0, 0, 255, 0), thickness=2, lineType=8, shift=0)
+            print "good feature at", x, y 
+           
+        window_name = self.type
+        cv.NamedWindow(window_name, cv.CV_WINDOW_AUTOSIZE)                   
+        cv.ShowImage(window_name, imgColour) #Show the image
+        cv.waitKey()        
+        
+    def peopleDetect(self):
+        img = imread(self.imagePath)
+        hog = HOGDescriptor()
+        hog.setSVMDetector(HOGDescriptor.getDefaultPeopleDetector())
+        for r in hog.detectMultiScale(img, 0, Size(8,8), Size(24,16), 1.05, 2):
+            r.x += round(r.width*0.1)
+            r.y += round(r.height*0.1)
+            r.width = round(r.width*0.8)
+            r.height = round(r.height*0.8)
+            rectangle(img, r.tl(), r.br(), Scalar(0,255,0), 1)
+
+        namedWindow("people detector", 1)
+        imshow("people detector", img)
+        waitKey(0)
 
 if __name__ == '__main__':
     print "OpenCV Learning Application"
@@ -59,6 +89,9 @@ if __name__ == '__main__':
         print "Learning mode"
         recognition = NaiveRecognition(options.runType, args[0], args[1])
         
-    recognition.showImage()
+    # recognition.showImage()
+    # recognition.printGoodFeatures()
+    recognition.peopleDetect()
        
     
+
