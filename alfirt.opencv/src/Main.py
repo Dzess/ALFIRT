0c0ab66b21abae860bc63a578a5b26e4922b5b43
Mikolaj Dobski <ankhazam@gmail.com>
2011-07-12 20:18:44 +0200

    first compliance with opencv 2.3.1

--- alfirt.opencv/src/Main.py
+++ alfirt.opencv/src/Main.py
@@ -4,7 +4,7 @@ Created on 05-05-2011
 @author: Ankhazam
 '''
 
-import cv
+import cv2
 from optparse import OptionParser
 import sys
 import ImageDescription
@@ -27,25 +27,25 @@ class NaiveRecognition(object):
                 
     def showImage(self):
         window_name = self.type
-        cv.NamedWindow(window_name, cv.CV_WINDOW_AUTOSIZE)
-        image = cv.LoadImage(self.imagePath, cv.CV_LOAD_IMAGE_COLOR) #Load the image
-        cv.ShowImage(window_name, image) #Show the image
-        cv.WaitKey()
+        cv2.namedWindow(window_name, cv2.CV_WINDOW_AUTOSIZE)
+        image = cv2.imread(self.imagePath)
+        cv2.imshow(window_name, image) #Show the image
+        cv2.waitKey()
         
     def printGoodFeatures(self):
-        img = cv.LoadImageM(self.imagePath, cv.CV_LOAD_IMAGE_GRAYSCALE)
-        imgColour = cv.LoadImageM(self.imagePath)
-        eig_image = cv.CreateMat(img.rows, img.cols, cv.CV_32FC1)
-        temp_image = cv.CreateMat(img.rows, img.cols, cv.CV_32FC1)
+        img = cv2.cv.LoadImageM(self.imagePath, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+        imgColour = cv2.cv.LoadImageM(self.imagePath)
+        eig_image = cv2.cv.CreateMat(img.rows, img.cols, cv2.CV_32FC1)
+        temp_image = cv2.cv.CreateMat(img.rows, img.cols, cv2.CV_32FC1)
         
-        for (x, y) in cv.GoodFeaturesToTrack(img, eig_image, temp_image, 100, 0.04, 1.0, useHarris=True):
-            cv.Circle(imgColour, (int(x), int(y)), 3, cv.Scalar(0, 0, 255, 0), thickness=2, lineType=8, shift=0)
+        for (x, y) in cv2.cv.GoodFeaturesToTrack(img, eig_image, temp_image, 100, 0.04, 1.0, useHarris=True):
+            cv2.cv.Circle(imgColour, (int(x), int(y)), 3, cv2.cv.Scalar(0, 0, 255, 0), thickness=2, lineType=8, shift=0)
             print "good feature at", x, y 
            
         window_name = self.type
-        cv.NamedWindow(window_name, cv.CV_WINDOW_AUTOSIZE)                   
-        cv.ShowImage(window_name, imgColour) #Show the image
-        cv.WaitKey()        
+        cv2.namedWindow(window_name, cv2.CV_WINDOW_AUTOSIZE)                  
+        cv2.cv.ShowImage(window_name, imgColour) #Show the image
+        cv2.cv.WaitKey()        
         
 
 if __name__ == '__main__':
